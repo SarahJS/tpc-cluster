c8b3262e4e0a60370bd99680d281d74483971362
SarahJS <sedky.sarah@gmail.com>
2017-07-29 15:12:47 -0700

    Added stuff to web.xml

--- Backend/primary/RESTInterface.java
+++ Backend/primary/RESTInterface.java
@@ -5,6 +5,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import java.io.IOException;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
@@ -33,9 +36,10 @@ public class RESTInterface {
 		myKVS.Remove(key);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// Pass in IPs of secondaries.
 		System.out.println("I LIVE");
+		System.in.read();
 	}
 	
 }
